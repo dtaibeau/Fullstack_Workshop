b4fe0731763ba1cea054bbb0276e3d3e7e44609d
Delphine Tai-Beauchamp <delphinetaibeauchamp@gmail.com>
2025-01-10 17:55:06 -0800

    added frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'react-bootstrap' 
+
+function App() {
+  return (
+    <>
+      <Button className="m-auto text-center" variant="primary">Button</Button>
+    </>
+  )
+}
+
+export default App
